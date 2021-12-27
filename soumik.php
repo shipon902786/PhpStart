d3115dd9c44c04164fdd0be0871bd768653b38d1
shipon <shipontalukdar902786@gmail.com>
2021-12-28 01:22:14 +0600

    first commit

---
+++ soumik.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$num1=10;
+$num2=20;
+
+$sum=$num1+$num2;
+$sub=$num1-$num2;
+$mul=$num1*$num2;
+$div=$num1/$num2;
+
+
+
+echo "<h1>Summetion Is: $sum</h1>";
+echo "<h1>Biyog Is: $sub</h1>";
+echo "<h1>Multiplication Is: $mul</h1>";
+echo "<h1>Multiplication Is: $div</h1>";
+
+
+
+
+?>
